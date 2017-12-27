3345daea582ac92027a692f92f00ec913e5ae2ae
user <user@user-PC10>
2017-12-27 16:31:46 +0900

    se

--- git/src/git/test.java
+++ git/src/git/test.java
@@ -4,7 +4,12 @@ public class test {
 	public static void main(String[] args) {
 
 		System.out.println("hello wolrd");
-	
+		System.out.println("hello wolrd");
+		System.out.println("hello wolrd");
+		System.out.println("hello wolrd");
+		System.out.println("hello wolrd");
+		System.out.println("hello wolrd");
+		
 		
 	}
 
